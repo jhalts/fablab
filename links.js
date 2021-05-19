e1db10d831c5eb8f16716f6f99d440eff2fcb2c4
jhalts <47040550+jhalts@users.noreply.github.com>
2021-05-19 11:11:53 +0300

    pakko

---
+++ links.js
@@ -0,0 +1,21 @@
+document.write('\
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>\
+<header>\
+<div id="headerItemsLeft">\
+    <li><a href="index.html">Home</a></li>\
+    <li class="dropdown">\
+        <a href="javascript:void(0)" class="dropbtn">Projects <i class="fas fa-caret-down"></i></a>\
+        <div class="dropdown-content">\
+            <a id="dropTop" href="flag.html">Flag</a>\
+            <a href="lamp.html">Lamp</a>\
+            <a href="laptopholder.html">Laptop Holder</a>\
+            <a id="dropBottom" href="gamepad.html">Gamepad</a>\
+        </div>\
+    </li>\
+</div>\
+<div id="headerItemsRight">\
+    <li class="some"><a href="https://www.linkedin.com/in/jukkahaltsonen/" target="_blank"><i class="fab fa-linkedin"></i></a></li>\
+</div>\
+</header>\
+<footer>&copy; 2021 Jukka Haltsonen</footer>\
+ ');
